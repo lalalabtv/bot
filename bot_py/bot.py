c4f9bd2e4715befd78f611731a9ad421d91f0ce6
lalalabtv <55194720+lalalabtv@users.noreply.github.com>
2019-09-11 23:58:31 +0300

    Update bot.py

--- bot_py/bot.py
+++ bot_py/bot.py
@@ -60,7 +60,7 @@ while True:
                             number = event.text
                             vk_session.method('messages.send',
                                               {'user_id': event.user_id, 'message': 'Проверяем...', 'random_id': 0})
-                            for x in list(partys.keys()):
+                            for x in list(partys):
                                 if x.own_number == number:
                                     partys.remove(x)
                             vk_session.method('messages.send',
