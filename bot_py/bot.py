100781fa184bf1cd4453495c0e65122846705d55
lalalabtv <55194720+lalalabtv@users.noreply.github.com>
2019-09-11 20:25:59 +0300

    Update bot.py

--- bot_py/bot.py
+++ bot_py/bot.py
@@ -15,7 +15,9 @@ class Pyanka(object):
         self.place = place
         self.persons = persons
 
-partys = [' ']
+p0 = Pyanka('null', 'test', 'null')
+
+partys = [p0]
 
 kolvo = 0
 
@@ -39,7 +41,7 @@ while True:
                                        'random_id': 0})
 
             if event.from_user and response == 'создать пьянку':
-                p = Pyanka
+                p: Type[Pyanka] = Pyanka
                 vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Введите дату', 'random_id': 0})
                 flag = 1
                 while flag == 1:
